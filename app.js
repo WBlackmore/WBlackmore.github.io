bd75080a8f2d3ac08cb4a5abed4785d30d0c6d61
Will Barrett <otterly456@outlook.com>
2022-12-31 22:09:59 +0000

    add media queries for smaller devices

--- app.js
+++ app.js
@@ -4,10 +4,13 @@ const icons = document.querySelectorAll(".fa-3x");
 for(let icon of icons){
     icon.addEventListener('mouseover', ()=> {
         icon.style.color="maroon";
+        icon.style.height="120%"
         
     })
     icon.addEventListener('mouseout', ()=> {
         icon.style.color="antiquewhite";
+        icon.style.height="100%"
     })
 
 }
+
